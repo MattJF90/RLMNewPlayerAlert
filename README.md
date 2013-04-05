f09348f1d55521324df27d73c0ea16a435b8fbd8
Matt Fielding <mattfielding@cogeco.ca>
2013-04-05 11:53:23 -0400

    Project created

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-rlmnewplayeralert
+RLMNewPlayerAlert
 =================
 
 RLMNewPlayerAlert
---
+++ src/com/rlminecraft/RLMNewPlayerAlert/RLMNewPlayerAlert.java
@@ -0,0 +1,16 @@
+package com.rlminecraft.RLMNewPlayerAlert;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RLMNewPlayerAlert extends JavaPlugin {
+	
+	public void onEnable () {
+		
+	}
+	
+	
+	public void onDisable () {
+		
+	}
+	
+}
